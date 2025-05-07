54ba3522ddd940f41209f607ebdc04444902b3a7
Aniruddh <noirrr@duck.com>
2025-05-07 07:19:42 +0000

    Switched to tokio

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 #[allow(unused_imports)]
-use std::net::UdpSocket;
+use tokio::net::UdpSocket;
+// use std::net::UdpSocket;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
-    let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
-    let mut buf = [0; 512];
+    let udp_socket = UdpSocket::bind("127.0.0.1:2053")
+        .await
+        .expect("Failed to bind to address");
+    let mut buf = [0u8; 512];
 
     loop {
-        match udp_socket.recv_from(&mut buf) {
+        match udp_socket.recv_from(&mut buf).await {
             Ok((size, source)) => {
                 println!("Received {} bytes from {}", size, source);
                 let response = [];
                 udp_socket
                     .send_to(&response, source)
+                    .await
                     .expect("Failed to send response");
             }
             Err(e) => {
