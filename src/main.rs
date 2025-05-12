831224a204105cbcbd7809aef65091ab9f05af6b
Aniruddh <noirrr@duck.com>
2025-05-12 23:23:03 +0000

    Made a DNSHeader struct

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,30 @@
 use std::{net::SocketAddr, sync::Arc};
 use tokio::net::UdpSocket;
 
+#[derive(Debug)]
+struct DNSHeader {
+    id: u16,
+    flags: u16,
+    qdcount: u16,
+    ancount: u16,
+    nscount: u16,
+    arcount: u16,
+}
+
+impl DNSHeader {
+    pub fn new(header: &[u8]) -> Self {
+        assert_eq!(header.len(), 12);
+        DNSHeader {
+            id: u16::from_be_bytes([header[0], header[1]]),
+            flags: u16::from_be_bytes([header[2], header[3]]),
+            qdcount: u16::from_be_bytes([header[4], header[5]]),
+            ancount: u16::from_be_bytes([header[6], header[7]]),
+            nscount: u16::from_be_bytes([header[8], header[9]]),
+            arcount: u16::from_be_bytes([header[10], header[11]]),
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -32,6 +56,12 @@ async fn main() {
 
 async fn process(socket: Arc<UdpSocket>, buffer: &[u8], address: SocketAddr) {
     eprintln!("Received {} bytes from {}", buffer.len(), address);
+
+    assert!(buffer.len() >= 12);
+
+    let header = DNSHeader::new(&buffer[..12]);
+
+    eprintln!("Received header: {:#?}", header);
 
     let response = [];
     socket
